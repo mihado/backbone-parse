da5aa095ae64dea83e3856f007df08d106fd6511
pete <peterjacobson.nz@gmail.com>
2016-07-26 10:12:05 +1200

    change credetials to global references

--- backbone-parse.js
+++ backbone-parse.js
@@ -1,20 +1,20 @@
 /********** PARSE API ACCESS CREDENTIALS **********/
 
-var application_id = "CkWCHMSOgyqoNKoIc5hu09uvdZcJ9rpHJD4iwhxI";
-var rest_api_key = "H5SIwarTRXqd07C0OIZPbcRTYTNLKsjFAJt5PrFY";
-var api_version = "1";
+var application_id = BuildProperties.parse_application_id;
+var rest_api_key = BuildProperties.parse_rest_key;
+var api_version = BuildProperties.parse_api_version;
 
 /******************* END *************************/
 
 (function() {
 
-    /*  
+    /*
         Replace the toJSON method of Backbone.Model with our version
-        
-        This method removes the "createdAt" and "updatedAt" keys from the JSON version 
+
+        This method removes the "createdAt" and "updatedAt" keys from the JSON version
         because otherwise the PUT requests to Parse fails.
      */
-	var original_toJSON =Backbone.Model.prototype.toJSON; 
+	var original_toJSON =Backbone.Model.prototype.toJSON;
     var ParseModel = {
         toJSON : function(options) {
             _parse_class_name = this.__proto__._parse_class_name;
@@ -28,13 +28,13 @@ var api_version = "1";
     };
     _.extend(Backbone.Model.prototype, ParseModel);
 
-    /*  
+    /*
         Replace the parse method of Backbone.Collection
 
         Backbone Collection expects to get a JSON array when fetching.
         Parse returns a JSON object with key "results" and value being the array.
     */
-    original_parse =Backbone.Collection.prototype.parse; 
+    original_parse =Backbone.Collection.prototype.parse;
     var ParseCollection = {
         parse : function(options) {
             _parse_class_name = this.__proto__._parse_class_name;
@@ -51,8 +51,8 @@ var api_version = "1";
     };
     _.extend(Backbone.Collection.prototype, ParseCollection);
 
-    /*  
-        Method to HTTP Type Map 
+    /*
+        Method to HTTP Type Map
     */
 	var methodMap = {
         'create': 'POST',
@@ -61,12 +61,12 @@ var api_version = "1";
         'read':   'GET'
     };
 
-    /* 
-        Override the default Backbone.sync 
+    /*
+        Override the default Backbone.sync
     */
     var ajaxSync = Backbone.sync;
 	Backbone.sync = function(method, model, options) {
-		
+
 
         var object_id = model.models? "" : model.id; //get id if it is not a Backbone Collection
 
@@ -84,7 +84,7 @@ var api_version = "1";
         if (method != "create") {
             url = url + object_id;
         }
-	   
+
         //Setup data
 		var data ;
 	    if (!options.data && model && (method == 'create' || method == 'update')) {
@@ -92,7 +92,7 @@ var api_version = "1";
 	    }
         else if (options.query && method == "read") { //query for Parse.com objects
             data = encodeURI("where=" + JSON.stringify(options.query));
-        }   
+        }
 
 		var request = {
             //data
